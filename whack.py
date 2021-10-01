fa7b6ff52708c37354144a99ebeccd885fd80bd9
Denis REMACLE <43983759+Denis-REMACLE@users.noreply.github.com>
2021-10-01 17:03:45 +0200

    Update whack.py

--- whack.py
+++ whack.py
@@ -80,7 +80,7 @@ def evil_twix(interface, target):
 
     # Allow forwarding and put interface in ip tables
     os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
-    os.system("iptables -I POSTROUTING -t nat –o wlan0 -j MASQUERADE")
+    os.system("iptables -I POSTROUTING -t nat -o wlan0 -j MASQUERADE")
     
     # Copy the templates in the working directory
     os.system("cp template/template_dnsmasq.conf dnsmasq.conf")
